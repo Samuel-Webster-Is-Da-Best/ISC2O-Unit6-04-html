4afe7174769577712f5fed6c1ef492515e626c29
SamuelWebsterIs <>
2021-06-07 13:56:45 +0000

    spaces

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('button').addEventListener('click', math)
 
 function math () {
-  //read values
+  // read values
   const height = document.getElementById('height').value
   const top = document.getElementById('top').value
   const bottom = document.getElementById('bottom').value
-  //check if its unacceptable
+  // check if its unacceptable
   if (height <= 0) {
     alert('you must use numbers above 0')
     return
@@ -18,7 +18,7 @@ function math () {
     alert('you must use numbers above 0')
     return
   }
-  //do math, return answer, and clear input boxes
+  // do math, return answer, and clear input boxes
   alert(top * height / 2 + bottom * height / 2)
   document.getElementById('height').value = ''
   document.getElementById('top').value = ''
