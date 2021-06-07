b5bb918f1ad0ba4c234cbc44c9f23e05d7d64242
SamuelWebsterIs <>
2021-06-07 13:51:34 +0000

    more

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 document.getElementById('button').addEventListener('click', math)
 
 function math () {
+  //read values
   const height = document.getElementById('height').value
   const top = document.getElementById('top').value
   const bottom = document.getElementById('bottom').value
+  //check if its unacceptable
   if (height <= 0) {
     alert('you must use numbers above 0')
-    return()
+    return
   }
   if (height <= 0) {
     alert('you must use numbers above 0')
-    return()
+    return
   }
   if (height <= 0) {
     alert('you must use numbers above 0')
-    return()
+    return
   }
+  //do math, return answer, and clear input boxes
   alert(top * height / 2 + bottom * height / 2)
   document.getElementById('height').value = ''
   document.getElementById('top').value = ''
